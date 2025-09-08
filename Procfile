7ef100d0b42e6a51df3ff5cc4a77e439d66998f0
wintan1418 <wintan1418@gmail.com>
2025-09-08 15:15:47 -0500

    Configure Railway deployment with Nixpacks (non-Docker)

---
+++ Procfile
@@ -0,0 +1,2 @@
+web: bundle exec rails db:prepare && bundle exec rails server -b 0.0.0.0 -p ${PORT:-3000}
+release: bundle exec rails db:migrate
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -18,7 +18,7 @@ Rails.application.configure do
 
   # Ensures that a master key has been made available in ENV["RAILS_MASTER_KEY"], config/master.key, or an environment
   # key such as config/credentials/production.key. This key is used to decrypt credentials (and other encrypted files).
-  # config.require_master_key = true
+  config.require_master_key = true
 
   # Enable serving static files from the `/public` folder (turn on if using Render.com or similar)
   config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present? || Rails.env.production?
